e7e96482f75d78bd4d2ec92a41e4550f9f3f3177
gerson <gerson@gerson.com>
2024-08-08 15:43:03 -0400

    Test rotation completed

--- World.gd
+++ World.gd
@@ -19,12 +19,13 @@ func get_random_floor_point() -> Vector3:
 func try_spawn(scene_name) -> Spatial:
 	var scene : Spatial = load(scene_name).instance()
 	add_child(scene)
-	
+	var rand_angle = [0,PI/2,PI/2*2,PI/2*3][randi()%3]
+	scene.global_rotation.y = rand_angle
 	for i in range(0,50):
-		var rand_pos = get_random_floor_point()
 		if i%3 == 0:
-			move_to_random_wall(scene)
+			move_to_random_corner(scene)
 		else:
+			var rand_pos = get_random_floor_point()
 			scene.global_translation = rand_pos
 		#Check for collision
 		if (!obj_is_valid_pos(scene)):
@@ -76,6 +77,30 @@ func move_to_random_wall(obj : Spatial):
 	obj_pos += rand_point
 	obj.global_translation = Vector3(obj_pos.x,0,obj_pos.y)
 
+
+func move_to_random_corner(obj : Spatial):
+	var corner : Array = get_random_corner()
+	var check : CollisionUtilities = CollisionUtilities.new()
+	var vertices = check.get_vertices(obj.get_node("CollisionShape"))
+	
+	# Check the dir of the wall
+	var corner_type = corner[1]
+	var obj_vertex = vertices[0]
+	if corner_type == 0:
+		pass
+	elif corner_type == 1:
+		obj_vertex = vertices[1]
+	elif corner_type == 2:
+		obj_vertex = vertices[2]
+	elif corner_type == 3:
+		obj_vertex = vertices[3]
+	
+	
+	var obj_pos = Vector2(obj.global_translation.x, obj.global_translation.z) - obj_vertex
+	obj_pos += corner[0]
+	obj.global_translation = Vector3(obj_pos.x,0,obj_pos.y)
+
+
 # Returns a segment that represent a wall
 func get_random_wall():
 	var walls = [
@@ -85,6 +110,16 @@ func get_random_wall():
 		[Vector2(0,0), Vector2(0,4), 3]
 	]
 	return walls[randi()%walls.size()]
+
+func get_random_corner():
+	var corners = [
+		[Vector2(0,0), 0],
+		[Vector2(4,0), 1],
+		[Vector2(4,4), 2],
+		[Vector2(0,4), 3]
+	]
+	return corners[randi()%corners.size()]
+
 
 func _ready():
 	randomize()
